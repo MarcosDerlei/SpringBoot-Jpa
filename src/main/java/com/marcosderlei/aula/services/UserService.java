f8678cb64915819519e702ce82d9dc3fd16657be
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2024-12-26 08:53:42 -0300

    Service layer, component registration

---
+++ src/main/java/com/marcosderlei/aula/services/UserService.java
@@ -0,0 +1,26 @@
+package com.marcosderlei.aula.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosderlei.aula.entities.User;
+import com.marcosderlei.aula.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User>fidAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
