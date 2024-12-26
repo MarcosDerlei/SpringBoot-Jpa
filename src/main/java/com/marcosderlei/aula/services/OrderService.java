0baa5933dc626ed7f5e7c6dc97e43b836cae23ac
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2024-12-26 11:26:43 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/marcosderlei/aula/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.marcosderlei.aula.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosderlei.aula.entities.Order;
+import com.marcosderlei.aula.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> fidAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
