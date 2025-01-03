e52f1bdf172b6c4cd06f5d86d87855e176fbe8c0
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2025-01-03 09:00:42 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/marcosderlei/aula/services/ProductService.java
+++ src/main/java/com/marcosderlei/aula/services/ProductService.java
@@ -5,23 +5,25 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marcosderlei.aula.entities.Product;
 import com.marcosderlei.aula.repositories.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
 
-	public List<Product>findAll() {
+	@Transactional(readOnly = true)
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
+	@Transactional(readOnly = true)
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new RuntimeException("Product not found"));
 	}
-
 }
