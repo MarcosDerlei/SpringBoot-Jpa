43e8c33ec88ab9ae75506c47258d1956d82ba4cf
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2025-01-13 09:17:44 -0300

    Exception handling - findById

--- src/main/java/com/marcosderlei/aula/services/UserService.java
+++ src/main/java/com/marcosderlei/aula/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcosderlei.aula.entities.User;
 import com.marcosderlei.aula.repositories.UserRepository;
+import com.marcosderlei.aula.services.exceptions.ResourceNotFoundException;
 
 import jakarta.transaction.Transactional;
 
@@ -23,7 +24,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
---
+++ src/main/java/com/marcosderlei/aula/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.marcosderlei.aula.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
