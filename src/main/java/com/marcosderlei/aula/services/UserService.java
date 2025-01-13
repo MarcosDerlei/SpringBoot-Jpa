c3de7369ecdddc2cbf2ee7ff7c3d12fb1aca6f87
Marcos Derlei Meyer <marcos.derlei85@gmail.com>
2025-01-13 11:12:26 -0300

    Exception handling - update

--- src/main/java/com/marcosderlei/aula/services/UserService.java
+++ src/main/java/com/marcosderlei/aula/services/UserService.java
@@ -13,6 +13,7 @@ import com.marcosderlei.aula.repositories.UserRepository;
 import com.marcosderlei.aula.services.exceptions.DatabaseException;
 import com.marcosderlei.aula.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -46,9 +47,13 @@ public class UserService {
 
 	@Transactional
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
